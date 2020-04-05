f6f106d9af116a3612257facfd6c5a8a4395217c
Pierre-Noel Bouteville <pnb990@gmail.com>
2020-04-05 17:31:22 +0200

    continue import

--- PnbPartKeepr/models.py
+++ PnbPartKeepr/models.py
@@ -15,8 +15,10 @@ def get_default_user_id():
 ###############################################################################
 
 class Category(MPTTModel):
+    class Meta:
+        abstract = True
+
     name = models.CharField(
-            unique=True,
             blank=False,
             help_text='name',
             max_length=255
@@ -34,9 +36,6 @@ class Category(MPTTModel):
             )
     class MPTTMeta:
         order_insertion_by = ['name']
-
-    class Meta:
-        abstract = True
 
 
 class PartCategory(Category):
@@ -104,6 +103,9 @@ class Footprint(models.Model):
 ###############################################################################
 
 class Company(models.Model):
+    class Meta:
+        abstract = True
+
     UPLOAD_TO=None
 
     name = models.CharField(
@@ -142,8 +144,6 @@ class Company(models.Model):
             blank=True,
             help_text='Image'
             )
-    class Meta:
-        abstract = True
 
 class Distributor(Company):
     UPLOAD_TO = 'distributor/images/%Y/%m/%d/'
@@ -160,7 +160,46 @@ class Distributor(Company):
 class Manufacturer(models.Model):
     UPLOAD_TO = 'manufacturer/images/%Y/%m/%d/'
 
+###############################################################################
+# Unit
+###############################################################################
 
+class SiPrefix(models.Model):
+    prefix = models.CharField(
+            help_text='System International prefix name (e.g. yotta, deca, deci, centi)',
+            max_length=255
+            )
+    symbol = models.CharField(
+            help_text='Symbol of System International prefix (e.g. m, M, G)',
+            max_length=255
+            )
+    exponent = models.IntegerField(
+            help_text='The exponent of the System International prefix (e.g. milli = 10^-3)'
+            )
+    base = models.IntegerField(
+            help_text='The base of the System International (e.g. 2^-3)'
+            )
+
+    """
+    Calculates the product for a given value.
+    @param $value float The value to calculate the product
+    @return float The resulting value
+    """
+    def calculateProduct(value):
+        return value * self.base**self.exponent
+
+class Unit(models.Model):
+    name = models.CharField(
+            help_text='The name of the unit (e.g. Volts, Ampere, Farad, Metres)',
+            max_length=255
+            )
+    symbol = models.CharField(
+            help_text='The symbol of the unit (e.g. V, A, F, m)',
+            max_length=255
+            )
+    prefixes = models.ManyToManyField(SiPrefix,
+             help_text='Defines the allowed System International prefix for this parameter unit'
+             )
 
 ###############################################################################
 # Part 
@@ -198,6 +237,57 @@ class Part(models.Model):
             on_delete=models.PROTECT, 
             blank=False,
             help_text='Storage location'
+            )
+    comment = models.TextField(
+            blank=True,
+            help_text='Some comments'
+            )
+
+    minStockLevel = models.PositiveIntegerField(
+            help_text='Number minimum of part in stock allowed'
+            )
+
+    averagePrice = models.DecimalField(
+            max_digits=13,
+            decimal_places=4,
+            null=True,
+            help_text='General average part\'s price'
+            )
+    status = models.CharField(
+            blank=True,
+            help_text='a status ... may be active/end of life/development ???',
+            max_length=255
+            )
+    needReview = models.BooleanField(
+            default=True,
+            help_text='Use this one for pricing calculation'
+            )
+    partCondition = models.CharField(
+            blank=True,
+            help_text='I don\' know...',
+            max_length=255
+            )
+    createDate = models.DateTimeField(
+            default=timezone.now
+            )
+    internalPartNumber = models.CharField(
+            blank=True,
+            help_text='part number used in internal data base',
+            max_length=255
+            )
+    removals = models.BooleanField(
+            default=False,
+            help_text='Need to be removed ?'
+            )
+
+    productionRemarks = models.CharField(
+            blank=True,
+            help_text='Remarks done by production',
+            max_length=255
+            )
+    metaPart = models.BooleanField(
+            default=False,
+            help_text='is a meta part ?'
             )
 
 class PartDistributor(models.Model):
@@ -263,13 +353,268 @@ class PartManufacturer(models.Model):
             )
 
 
+###############################################################################
+# Project
+###############################################################################
+
+class ProjectPart(models.Model): 
+
+    # TODO check utility PNB
+    OVERAGE_TYPE_ABSOLUTE = "absolute";
+    OVERAGE_TYPE_PERCENT = "percent";
+    OVERAGE_TYPE_CHOICES = [
+            (OVERAGE_TYPE_ABSOLUTE, 'absolute'),
+            (OVERAGE_TYPE_PERCENT,  'percent' )
+            ]
+
+    part = models.ForeignKey(
+            Part, 
+            blank=False,
+            on_delete=models.PROTECT, 
+            help_text='part'
+            )
+
+    quantity = models.PositiveIntegerField(
+            help_text='Part quantity inside'
+            )
+            
+    part = models.ForeignKey(
+            Part, 
+            blank=False,
+            on_delete=models.PROTECT, 
+            help_text='part'
+            )
+
+    remarks = models.CharField(
+            blank=True,
+            help_text='Remarks',
+            max_length=255
+            )
+
+    overageType = models.CharField(
+            blank=False,
+            help_text='The type of the value',
+            max_length=10,
+            choices=OVERAGE_TYPE_CHOICES
+            )
+
+    overage = models.PositiveIntegerField(
+            default=0,
+            help_text='the overage can either be percent or an absolute value depending on overageType.'
+            )
+
+    lotNumber = models.CharField(
+            blank=True,
+            help_text='the lot number',
+            max_length=10
+            )
+
+    # TODO Kept but i don't know what it is. PNB
+    totalQuantity = models.PositiveIntegerField(
+            default=0,
+            help_text='The total quantity including overage',
+            )
+
+
+class Project(models.Model):
+
+    name = models.CharField(
+            help_text='name',
+            max_length=255
+            )
+
+    owner = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            related_name='+',
+            null=True,
+            on_delete=models.SET_NULL,
+            default=get_default_user_id
+            )
+
+    description = models.CharField(
+            blank=True,
+            help_text='description',
+            max_length=255
+            )
+
+class ProjectRun(models.Model):
+
+    runDateTime = models.DateTimeField(
+            default=timezone.now
+            )
+
+    project = models.ForeignKey(
+            Project, 
+            on_delete=models.CASCADE, 
+            help_text='project'
+            )
+
+    quantity = models.PositiveIntegerField(
+            default=0,
+            help_text='The quantity project has been build',
+            )
+
+
+class ProjectRunPart(models.Model):
+
+    projectRun = models.ForeignKey(
+            ProjectRun, 
+            on_delete=models.CASCADE, 
+            help_text='project run'
+            )
+
+    part = models.ForeignKey(
+            Part, 
+            on_delete=models.PROTECT, 
+            help_text='the part used in a production run'
+            )
+
+    quantity = models.PositiveIntegerField(
+            help_text='the quantity of a production run'
+            )
+
+    lotNumber = models.CharField(
+            blank=True,
+            help_text='the lot number use in production',
+            max_length=10
+            )
+
+###############################################################################
+# Parameter
+###############################################################################
+
+class Parameter(models.Model):
+    class Meta:
+        abstract = True
+
+    TYPE_NUMERIC    = 'numeric'
+    TYPE_STRING     = 'string'
+    TYPE_CHOICES = (
+        (TYPE_NUMERIC   , 'numeric' ),
+        (TYPE_STRING    , 'string'  ),
+    )
+    
+    value   = models.FloatField(
+            help_text='value'
+            )
+
+    normalizedValue = models.FloatField(
+            blank=True,
+            help_text="normalized value"
+            )
+
+    siPrefix = models.ForeignKey(
+            SiPrefix,
+            related_name='+',
+            on_delete=models.PROTECT,
+            help_text='The SiPrefix of the unit'
+            )
+
+    stringValue = models.CharField(
+            blank=True,
+            help_text='Value in case of string',
+            max_length=255
+            )
+
+    valueType = models.CharField(
+            blank=True,
+            help_text='The type of the value',
+            max_length=10,
+            choices=TYPE_CHOICES
+            )
+
+    unit = models.ForeignKey(
+            Unit,
+            blank=True,
+            on_delete=models.CASCADE, 
+            help_text="The unit for this type. May be null"
+            )
+
+
+
+
+class PartParameter(models.Model):
+    part = models.ForeignKey(
+            Part, 
+            on_delete=models.CASCADE, 
+            help_text='parameter criteria ???'
+            )
+
+    name = models.CharField(
+            help_text='name',
+            max_length=255
+            )
+
+    description = models.CharField(
+            help_text='description',
+            max_length=255
+            )
+
+    minValue   = models.FloatField(
+            help_text='minimum value'
+            )
+
+    normalizedMinValue = models.FloatField(
+            blank=True,
+            help_text="normalized minimum value"
+            )
+
+    minSiPrefix = models.ForeignKey(
+            SiPrefix,
+            related_name='+',
+            on_delete=models.PROTECT,
+            help_text='The SiPrefix of the unit for minimum value'
+            )
+
+    maxValue   = models.FloatField(
+            help_text='maximum value'
+            )
+
+    normalizedMaxValue = models.FloatField(
+            blank=True,
+            help_text="normalized maximum value"
+            )
+
+    maxSiPrefix = models.ForeignKey(
+            SiPrefix,
+            related_name='+',
+            on_delete=models.PROTECT,
+            help_text='The SiPrefix of the unit for maximum value'
+            )
+
+
+
+class MetaPartParameterCriteria(Parameter):
+
+    TYPE_NUMERIC    = 'numeric'
+    TYPE_STRING     = 'string'
+    TYPE_CHOICES = (
+        (TYPE_NUMERIC   , 'numeric' ),
+        (TYPE_STRING    , 'string'  ),
+    )
+    
+    part = models.ForeignKey(
+            Part, 
+            on_delete=models.CASCADE, 
+            help_text='parameter criteria ???'
+            )
+    name = models.CharField(
+            help_text='name',
+            max_length=255
+            )
+    operator = models.CharField(
+            help_text='operator ???',
+            max_length=255
+            )
+
 
 ###############################################################################
 # Stock
 ###############################################################################
 
 class StockEntry(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+    owner = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
             related_name='+',
             null=True,
             on_delete=models.SET_NULL,
@@ -302,6 +647,9 @@ class StockEntry(models.Model):
 # Attachment
 ###############################################################################
 class Attachment(models.Model):
+    class Meta:
+        abstract = True
+
     UPLOAD_TO = None
     filename = models.CharField(
             blank=False,
@@ -318,11 +666,18 @@ class Attachment(models.Model):
             help_text='Footprint attachment content'
             )
 
-    class Meta:
-        abstract = True
+class ProjectAttachment(Attachment):
+    UPLOAD_TO = 'project/attachments/%Y/%m/%d/' 
+
+    project = models.ForeignKey(
+            Project, 
+            on_delete=models.CASCADE, 
+            help_text='project'
+            )
+
 
 class PartAttachment(Attachment):
-    UPLOAD_TO = 'attachment/images/%Y/%m/%d/' 
+    UPLOAD_TO = 'part/attachments/%Y/%m/%d/' 
 
     part = models.ForeignKey(
             Part, 
@@ -331,7 +686,7 @@ class PartAttachment(Attachment):
             )
 
 class FootprintAttachment(Attachment):
-    UPLOAD_TO = 'attachment/images/%Y/%m/%d/' 
+    UPLOAD_TO = 'footprint/attachments/%Y/%m/%d/' 
 
     footprint = models.ForeignKey(
             Footprint, 
--- importFromParkKeepr.py
+++ importFromParkKeepr.py
@@ -38,11 +38,76 @@ import django
 django.setup()
 from PnbPartKeepr import models
 
-cursor.execute("SELECT  `id`,  `name`,  LEFT(`address`, 256),  `url`,  `phone`,  `fax`,  `email`,  LEFT(`comment`, 256),  `skuurl`,  `enabledForReports` FROM `distributor` LIMIT 1000;")
+
+###############################################################################
+# Category
+###############################################################################
+
+for table,obj in [
+        ('footprintcategory',models.FootprintCategory),
+        ('partcategory',models.PartCategory),
+        ('storagelocationcategory',models.StorageLocationCategory)
+        ]:
+    print("importing {} --------------------------------------------".format(table))
+    cursor.execute("SELECT  `id`,  `parent_id`,  `lft`,  `rgt`,  `lvl`,  `root`,  `name`,  `description`,  `categoryPath` FROM `{}`;".format(table))
+    for id,parent_id,lft,rgt,lvl,root,name,description,categoryPath in cursor:
+        category,created = obj.objects.get_or_create(id=id)
+        if created:
+            print("    {} id {} name {} created".format(table,id,name))
+        category.parent_id      = parent_id
+        category.lft            = lft
+        category.rght           = rgt
+        category.level          = lvl
+        category.tree_id        = root
+        category.name           = name
+        category.description    = description if description != None else ''
+        category.save()
+
+###############################################################################
+# StorageLocation
+###############################################################################
+
+print("importing {} --------------------------------------------".format('storagelocation'))
+cursor.execute("SELECT  `id`,  `category_id`,  `name` FROM `storagelocation`;")
+for id,category_id,name in cursor:
+    category = models.StorageLocationCategory.objects.get(id=category_id)
+
+    storagelocation,created = models.StorageLocation.objects.get_or_create(id=id,category=category)
+    if created:
+        print("    StorageLocation id {} name {} created".format(id,name))
+    storagelocation.category    = category
+    storagelocation.name        = name
+    storagelocation.save()
+
+
+###############################################################################
+# Footprint
+###############################################################################
+
+print("importing {} --------------------------------------------".format('footprint'))
+cursor.execute("SELECT  `id`,  `category_id`,  `name`,  `description` FROM `footprint`;")
+for id,category_id,name,description in cursor:
+    if category_id == None:
+        category_id = 1
+    category = models.FootprintCategory.objects.get(id=category_id)
+    footprint,created = models.Footprint.objects.get_or_create(id=id,name=name,category=category)
+    if created:
+        print("Footprint id {} name {} created".format(id,name))
+    footprint.name          = name
+    footprint.description   = description
+    footprint.category      = category
+
+
+###############################################################################
+# Company
+###############################################################################
+
+print("importing {} --------------------------------------------".format('distributor'))
+cursor.execute("SELECT  `id`,  `name`,  `address`,  `url`,  `phone`,  `fax`,  `email`,  `comment`,  `skuurl`,  `enabledForReports` FROM `distributor` ")
 for id,name,address,url,phone,fax,email,comment,skuurl,enabledForReports in cursor:
     distributor,created = models.Distributor.objects.get_or_create(id=id)
     if created:
-        print("Distributor id {} name {} created".format(id,name))
+        print("    Distributor id {} name {} created".format(id,name))
     distributor.name        = name
     distributor.address     = name
     distributor.url         = url
@@ -54,23 +119,84 @@ for id,name,address,url,phone,fax,email,comment,skuurl,enabledForReports in curs
     distributor.forReports  = enabledForReports
     distributor.save()
 
-cursor.execute("SELECT  `id`,  `parent_id`,  `lft`,  `rgt`,  `lvl`,  `root`,  `name`,  LEFT(`description`, 256),  LEFT(`categoryPath`, 256) FROM `footprintcategory` LIMIT 1000;")
-for id,parent_id,lft,rgt,lvl,root,name,description,categoryPath in cursor:
-    category,created = models.FootprintCategory.objects.get_or_create(id=id)
+print("importing {} --------------------------------------------".format('manufacturer'))
+cursor.execute("SELECT  `id`,  `name`,  `address`,  `url`,  `phone`,  `fax`,  `email`,  `comment` FROM `manufacturer` ")
+for id,name,address,url,phone,fax,email,comment in cursor:
+    manufacturer,created = models.Manufacturer.objects.get_or_create(id=id)
     if created:
-        print("category id {} name {} created".format(id,name))
-    category.parent_id      = parent_id
-    category.lft            = lft
-    category.rght           = rgt
-    category.level          = lvl
-    category.tree_id        = root
-    category.name           = name
-    category.description    = description if description != None else ''
-    category.save()
+        print("    Manufacturer id {} name {} created".format(id,name))
+    manufacturer.name        = name
+    manufacturer.address     = name
+    manufacturer.url         = url
+    manufacturer.phone       = phone
+    manufacturer.fax         = fax
+    manufacturer.email       = email
+    manufacturer.comment     = comment
+    manufacturer.save()
 
-#cursor.execute("SELECT  `id`,  `category_id`,  `name`,  LEFT(`description`, 256) FROM `testdb`.`footprint` LIMIT 1000;")
-#for id,category_id,name,description in cursor:
-#    footprint,created = models.Footprint.objects.get_or_create(id=id)
-    print("Footprint id {} name {} created".format(id,name))
-#    footprint.name        = name
+###############################################################################
+# Unit
+###############################################################################
+
+print("importing {} --------------------------------------------".format('siPrefix'))
+cursor.execute("SELECT  `id`,  `prefix`,  `symbol`,  `exponent`,  `base` FROM `siprefix` ")
+for id,prefix,symbol,exponent,base in cursor:
+    siprefix,created = models.SiPrefix.objects.get_or_create(
+            id=id,
+            exponent=exponent,
+            base=base
+            )
+    if created:
+        print("    SiPrefix id {} siprefix {} {} created".format(id,prefix,symbol))
+    siprefix.prefix     = prefix
+    siprefix.symbol     = symbol
+    siprefix.exponent   = exponent
+    siprefix.base       = base
+    siprefix.save()
+
+print("importing {} --------------------------------------------".format('unit'))
+cursor.execute("SELECT  `id`,  `name`,  `symbol`  FROM `unit` ")
+for id,name,symbol in cursor:
+    unit,created = models.Unit.objects.get_or_create(id=id)
+    if created:
+        print("    Unit id {} name {} created".format(id,name))
+    siprefix.name       = name
+    siprefix.symbol     = symbol
+    siprefix.save()
+
+print("importing {} --------------------------------------------".format('unit/siPrefix link'))
+cursor.execute("SELECT  `unit_id`,  `siprefix_id`  FROM `unitsiprefixes` ")
+for unit_id,siprefix_id in cursor:
+    unit = models.Unit.objects.get(     id=unit_id      )
+    siprefix = models.SiPrefix.objects.get( id=siprefix_id  )
+    unit.prefixes.add(siprefix)
+    
+
+###############################################################################
+# Part 
+###############################################################################
+
+print("importing {} --------------------------------------------".format('part'))
+cursor.execute("SELECT  `id`,  `category_id`,  `footprint_id`,  `name`,  `description`,  `comment`,  `stockLevel`,  `minStockLevel`,  `averagePrice`,  `status`,  `needsReview`,  `partCondition`,  `createDate`,  `internalPartNumber`,  `removals`,  `lowStock`,  `partUnit_id`,  `storageLocation_id`,  `productionRemarks`,  `metaPart` FROM `part` ;")
+for id, name, category_id, footprint_id, name, description, comment, stockLevel, minStockLevel, averagePrice, status, needsReview, partCondition, createDate, internalPartNumber, removals, lowStock, partUnit_id, storageLocation_id, productionRemarks, metaPart in cursor:
+
+    category        = models.PartCategory.objects.get(      id=category_id          )
+    footprint       = models.Footprint.objects.get(         id=footprint_id         )
+    storagelocation = models.StorageLocation.objects.get(   id=storageLocation_id   )
+
+    part,created = models.Manufacturer.objects.get_or_create(
+            id=id,
+            category=category,
+            footprint=footprint
+            )
+    if created:
+        print("    Part id {} name {} created".format(id,name))
+    part.name               = name
+    part.description        = description
+    part.category           = category
+    part.footprint          = footprint
+    part.storagelocation    = storagelocation
+    part.comment            = comment
+    part.save()
+
 
